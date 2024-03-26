4861414292ee1a6a658c7d1e156d421fc77dd002
asnafialkaromi <asnafialkaromi4@gmail.com>
2024-03-26 17:10:28 +0700

    Finishing edit button function and make validate for each edit text

--- app/src/main/java/com/nafi/sfoods/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/nafi/sfoods/presentation/profile/ProfileFragment.kt
@@ -1,24 +1,65 @@
 package com.nafi.sfoods.presentation.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.nafi.sfoods.R
 import com.nafi.sfoods.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    private lateinit var binding : FragmentProfileBinding
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        binding = FragmentProfileBinding.inflate(layoutInflater, container,false)
+        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickActionSave()
+    }
+
+    private fun setClickActionSave() {
+        val editTextUsername = binding.textEditUsername
+        val editTextEmail = binding.textEditEmail
+        val editTextPhoneNumber = binding.textEditPhoneNumber
+
+        binding.layoutHeaderProfile.ivEdit.setOnClickListener {
+            if (editTextUsername.isEnabled) {
+                val username = editTextUsername.text.toString()
+                val email = editTextEmail.text.toString()
+                val phoneNumber = editTextPhoneNumber.text.toString()
+
+                if (username.isNotEmpty() && email.isNotEmpty() && phoneNumber.isNotEmpty()) {
+                    binding.layoutHeaderProfile.ivEdit.setImageResource(R.drawable.ic_edit)
+                    editTextUsername.isEnabled = false
+                    editTextEmail.isEnabled = false
+                    editTextPhoneNumber.isEnabled = false
+                } else {
+                    if (username.isEmpty()) {
+                        binding.textEditUsername.error = "Username tidak boleh kosong"
+                    }
+                    if (email.isEmpty()) {
+                        binding.textEditEmail.error = "Email tidak boleh kosong"
+                    }
+                    if (phoneNumber.isEmpty()) {
+                        binding.textEditPhoneNumber.error = "No. Telepon tidak boleh kosong"
+                    }
+                }
+            } else {
+                // Edit action
+                binding.layoutHeaderProfile.ivEdit.setImageResource(R.drawable.ic_save)
+                editTextUsername.isEnabled = true
+                editTextEmail.isEnabled = true
+                editTextPhoneNumber.isEnabled = true
+            }
+        }
+    }
 }
